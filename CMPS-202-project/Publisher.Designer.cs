9a52d61bc78e517d0b72dc4b8d893be76fa6a5da
Abdallah <abdallah.khafagy07@eng-st.cu.edu.eg>
2025-12-16 16:27:36 +0200

    updated publisher

--- CMPS-202-project/Publisher.Designer.cs
+++ CMPS-202-project/Publisher.Designer.cs
@@ -29,58 +29,37 @@
         private void InitializeComponent()
         {
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
             this.button2 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(205, 15);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox2.Location = new System.Drawing.Point(182, 12);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(156, 26);
+            this.textBox2.Size = new System.Drawing.Size(139, 22);
             this.textBox2.TabIndex = 1;
             this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
-            // 
-            // textBox3
-            // 
-            this.textBox3.Location = new System.Drawing.Point(205, 91);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(156, 26);
-            this.textBox3.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(48, -61);
+            this.label1.Location = new System.Drawing.Point(43, -49);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 20);
+            this.label1.Size = new System.Drawing.Size(0, 16);
             this.label1.TabIndex = 3;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.ForeColor = System.Drawing.Color.LavenderBlush;
-            this.label2.Location = new System.Drawing.Point(22, 99);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(146, 20);
-            this.label2.TabIndex = 4;
-            this.label2.Text = "Number Of episode";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(22, 22);
+            this.label3.Location = new System.Drawing.Point(20, 18);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(106, 20);
+            this.label3.Size = new System.Drawing.Size(90, 16);
             this.label3.TabIndex = 5;
             this.label3.Text = "Show\'s Name";
             // 
@@ -88,55 +67,62 @@
             // 
             this.button1.BackColor = System.Drawing.Color.LavenderBlush;
             this.button1.ForeColor = System.Drawing.Color.Black;
-            this.button1.Location = new System.Drawing.Point(205, 176);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Location = new System.Drawing.Point(182, 50);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 69);
+            this.button1.Size = new System.Drawing.Size(139, 55);
             this.button1.TabIndex = 6;
             this.button1.Text = "Add show";
             this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView1
             // 
             this.dataGridView1.BackgroundColor = System.Drawing.Color.LavenderBlush;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(580, 22);
-            this.dataGridView1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.dataGridView1.Location = new System.Drawing.Point(182, 153);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(270, 444);
+            this.dataGridView1.Size = new System.Drawing.Size(535, 261);
             this.dataGridView1.TabIndex = 7;
             // 
             // button2
             // 
             this.button2.BackColor = System.Drawing.Color.LavenderBlush;
             this.button2.ForeColor = System.Drawing.Color.Black;
-            this.button2.Location = new System.Drawing.Point(205, 311);
-            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Location = new System.Drawing.Point(12, 153);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 114);
+            this.button2.Size = new System.Drawing.Size(139, 91);
             this.button2.TabIndex = 8;
             this.button2.Text = "My Shows";
             this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(356, 12);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(173, 25);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "please enter name";
+            // 
             // PublisherForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MidnightBlue;
-            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
             this.ForeColor = System.Drawing.Color.LavenderBlush;
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "PublisherForm";
             this.Text = "Publisher";
             this.Load += new System.EventHandler(this.Publisher_Load);
@@ -148,12 +134,11 @@
 
         #endregion
         private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
     }
 }
--- CMPS-202-project/Publisher.cs
+++ CMPS-202-project/Publisher.cs
@@ -1,9 +1,11 @@
-﻿using System;
+﻿using DBapplication;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection.Emit;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -13,10 +15,13 @@ namespace CMPS_202_project
     public partial class PublisherForm : Form
     {
         string email;
+        Controller controllerObj = new Controller();
         public PublisherForm(string email)
         {
             InitializeComponent();
             this.email = email;
+            label4.Hide();
+            dataGridView1.AllowUserToAddRows = false;
             GUIHelper.ApplyModernStyle(this);
         }
 
@@ -32,12 +37,50 @@ namespace CMPS_202_project
 
         private void button2_Click(object sender, EventArgs e)
         {
+            DataTable dt1 = controllerObj.GetPublisherByEmail(email);
+            int id = Convert.ToInt32(dt1.Rows[0][0]);
 
+            DataTable dt2 = controllerObj.GetShowsByPublisherId(id);
+            if (dt2 != null)
+            {
+                if (dt2.Rows.Count == 0)
+                {
+                    MessageBox.Show("No shows found for you");
+                }
+                else
+                {
+                    dataGridView1.DataSource = dt2;
+                }
+            }
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label4.Hide();
+            if (string.IsNullOrEmpty(textBox2.Text))
+            {
+                label4.Show();
+                return;
+            }
+
+            DataTable dt1 = controllerObj.GetPublisherByEmail(email);
+            int publisherID = Convert.ToInt32(dt1.Rows[0][0]);
+            string showName = textBox2.Text;
+            // insert show
+            int status = controllerObj.InsertShow(publisherID, showName);
+            if (status == 0)
+            {
+                MessageBox.Show("Error adding show.");
+            }
+            else
+            {
+                MessageBox.Show("Show added successfully.");
+            }
         }
     }
 }
